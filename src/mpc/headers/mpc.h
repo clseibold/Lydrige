072a742ac91f8fde31eaef2534547fb494f63efe
Christian Seibold <musicchristian@outlook.com>
2016-05-08 09:23:14 -0500

    Updated mpc library

--- src/mpc/headers/mpc.h
+++ src/mpc/headers/mpc.h
@@ -81,6 +81,7 @@ typedef mpc_val_t*(*mpc_fold_t)(int,mpc_val_t**);
 */
 
 mpc_parser_t *mpc_new(const char *name);
+mpc_parser_t *mpc_copy(mpc_parser_t *a);
 mpc_parser_t *mpc_define(mpc_parser_t *p, mpc_parser_t *a);
 mpc_parser_t *mpc_undefine(mpc_parser_t *p);
 
@@ -274,6 +275,11 @@ mpc_ast_t *mpc_ast_state(mpc_ast_t *a, mpc_state_t s);
 void mpc_ast_delete(mpc_ast_t *a);
 void mpc_ast_print(mpc_ast_t *a);
 void mpc_ast_print_to(mpc_ast_t *a, FILE *fp);
+
+int mpc_ast_get_index(mpc_ast_t *ast, const char *tag);
+int mpc_ast_get_index_lb(mpc_ast_t *ast, const char *tag, int lb);
+mpc_ast_t *mpc_ast_get_child(mpc_ast_t *ast, const char *tag);
+mpc_ast_t *mpc_ast_get_child_lb(mpc_ast_t *ast, const char *tag, int lb);
 
 /*
 ** Warning: This function currently doesn't test for equality of the `state` member!
