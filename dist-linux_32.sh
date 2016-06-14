e4fd8f64034bd1d9c31fb4f3cf0aa1a3b8b20587
musicchristian@outlook.com <musicchristian@outlook.com>
2016-06-14 22:58:10 +0000

    added sh files to make distribution releases of 32-bit and 64-bit versions of program

---
+++ dist-linux_32.sh
@@ -0,0 +1,13 @@
+mkdir -p build/dist/x86
+
+mkdir -p build/dist/x86/mpc
+gcc -c -m32 -std=c99 -Wall -O3 src/mpc/c/mpc.c -o build/dist/x86/mpc/mpc.o
+ar -cvq build/dist/x86/libmpc.a build/dist/x86/mpc/*.o
+
+mkdir -p build/dist/x86/hashmap
+cd build/dist/x86/hashmap/
+gcc -m32 -c -std=c99 -Wall -O3 ../../../../src/hashmap/c/*.c
+ar -cvq ../libhashmap.a *.o
+cd ../../../../
+
+gcc -m32 -std=c99 -Wall -O3 src/main/c/*.c build/dist/x86/libmpc.a build/dist/x86/libhashmap.a -lreadline -lm -o build/dist/x86/main
