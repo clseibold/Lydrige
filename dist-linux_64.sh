6faa4542880ad8d72a675b904b62c4c9f8e5c4cf
musicchristian@outlook.com <musicchristian@outlook.com>
2016-06-26 22:44:30 +0000

    Updated dist-linux_*.sh files

--- dist-linux_64.sh
+++ dist-linux_64.sh
@@ -10,4 +10,8 @@ gcc -m64 -c -std=c99 -Wall -O3 ../../../../src/hashmap/c/*.c
 ar -cvq ../libhashmap.a *.o
 cd ../../../../
 
-gcc -m64 -std=c99 -Wall -O3 src/main/c/*.c build/dist/x86_64/libmpc.a build/dist/x86_64/libhashmap.a -lreadline -lm -o build/dist/x86_64/Lydrige-linux_x86_64_bin-v060a
+mkdir -p build/dist/x86_64/linenoise
+gcc -m64 -c -std=c99 -Wall -O3 src/linenoise/c/linenoise.c -o build/dist/x86_64/linenoise/linenoise.o
+ar -cvq build/dist/x86_64/liblinenoise.a build/dist/x86_64/linenoise/*.o
+
+gcc -m64 -std=c99 -Wall -O3 src/main/c/*.c build/dist/x86_64/libmpc.a build/dist/x86_64/libhashmap.a build/dist/x86_64/liblinenoise.a -lreadline -lm -o build/dist/x86_64/Lydrige-linux_x86_64_bin-v060a
