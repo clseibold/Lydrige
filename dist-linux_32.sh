ebc224df9cd65d200ed8fdc43aba99ea515c152e
Christian Seibold <musicchristian@outlook.com>
2016-06-26 17:44:30 -0500

    Updated dist-linux_*.sh files

--- dist-linux_32.sh
+++ dist-linux_32.sh
@@ -1,7 +1,7 @@
 mkdir -p build/dist/x86
 
 mkdir -p build/dist/x86/mpc
-gcc -c -m32 -std=c99 -Wall -O3 src/mpc/c/mpc.c -o build/dist/x86/mpc/mpc.o
+gcc -m32 -c -std=c99 -Wall -O3 src/mpc/c/mpc.c -o build/dist/x86/mpc/mpc.o
 ar -cvq build/dist/x86/libmpc.a build/dist/x86/mpc/*.o
 
 mkdir -p build/dist/x86/hashmap
@@ -10,4 +10,8 @@ gcc -m32 -c -std=c99 -Wall -O3 ../../../../src/hashmap/c/*.c
 ar -cvq ../libhashmap.a *.o
 cd ../../../../
 
-gcc -m32 -std=c99 -Wall -O3 src/main/c/*.c build/dist/x86/libmpc.a build/dist/x86/libhashmap.a -lreadline -lm -o build/dist/x86/Lydrige-linux_x86_bin-v060a
+mkdir -p build/dist/x86/linenoise
+gcc -m32 -c -std=c99 -Wall -O3 src/linenoise/c/linenoise.c -o build/dist/x86/linenoise/linenoise.o
+ar -cvq build/dist/x86/liblinenoise.a build/dist/x86/linenoise/*.o
+
+gcc -m32 -std=c99 -Wall -O3 src/main/c/*.c build/dist/x86/libmpc.a build/dist/x86/libhashmap.a build/dist/x86/liblinenoise.a -lreadline -lm -o build/dist/x86/Lydrige-linux_x86_bin-v060a
