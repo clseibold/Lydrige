6f640c57ceda6326a6f3c9c4acfe05868a8d3c2b
Christian Seibold <musicchristian@outlook.com>
2016-09-03 04:05:36 -0500

    Fixed another bug where errors weren't printed from prev expression until user hits enter at next prompt.

--- src/main/c/main.c
+++ src/main/c/main.c
@@ -246,7 +246,7 @@ int main(int argc, char** argv) { // TODO: Memory leak from not calling bdestroy
 				// mpc_ast_print((mpc_ast_t*) r.output); puts("");
 				dval *result = read_eval_expr(e, (mpc_ast_t *) r.output);
 				if (result->type == DVAL_ERROR) {
-					printf("Error: %s", result->str);
+					printf("Error: %s\n", result->str);
 				} else {
 					bool unknown = print_elem(*result, false);
 					printf("\n");
