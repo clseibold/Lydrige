4587264e8c9ecf2175d0c809ad280811d3464465
Christian Seibold <krixano@outlook.com>
2016-10-01 12:57:48 -0500

    Updated Windows build batch file

--- build_debug-win.bat
+++ build_debug-win.bat
@@ -1,32 +1,32 @@
 @echo off
-mkdir build
-mkdir build\debug
 
 set compiler=%1
 if /i "%compiler%"=="msvc" goto :msvc
 if /i "%compiler%"=="gcc" goto :gcc
 if /i "%compiler%"=="run" goto :run
-echo Please type in the compiler, msvc/gcc, or 'run'
+if /i "%compiler%"=="clean" goto :clean
+if /i "%compiler%"=="" echo Error: Please type in the compiler, 'msvc'/'gcc', or type in 'run' or 'clean'
+echo Error: Unknown command!
 goto :end
 
 :msvc
 set arch=%2
 if /i "%arch%"=="32" goto :msvc32
 if /i "%arch%"=="64" goto :msvc64
-echo Please type in '32'/'64' after the compiler argument.
+echo Error: Please type in '32'/'64' after the compiler argument.
 goto :end
 
 :msvc32
-cd build\debug
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 goto :msvcgo
 
 :msvc64
-cd build\debug
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 goto :msvcgo
 
 :msvcgo
+mkdir build\debug
+cd build\debug
 rem MSVC: Compile mpc
 cl /c ..\..\src\mpc\c\mpc.c
 lib mpc.obj
@@ -41,6 +41,7 @@ cd ..\..
 goto :end
 
 :gcc
+mkdir build\debug
 cd build\debug
 rem GCC: Compile mpc
 mkdir mpc
@@ -60,6 +61,15 @@ cd ..\..
 goto :end
 
 :run
+if not exist "build\debug\lydrige.exe" goto :runerror
 call build\debug\lydrige.exe
+goto :end
+
+:runerror
+echo Error: Must compile program before you can run it!
+goto :end
+
+:clean
+rmdir /S build
 
 :end
