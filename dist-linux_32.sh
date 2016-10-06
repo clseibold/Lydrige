86b5271152a0fb95c5614d0d8aa0b32a9100eee4
krixano@outlook.com <krixano@outlook.com>
2016-10-06 04:31:24 +0000

    Small Changes

--- dist-linux_32.sh
+++ dist-linux_32.sh
@@ -14,4 +14,4 @@ mkdir -p build/dist/x86/linenoise
 gcc -m32 -c -std=c99 -Wall -O3 src/linenoise/c/linenoise.c -o build/dist/x86/linenoise/linenoise.o
 ar -cvq build/dist/x86/liblinenoise.a build/dist/x86/linenoise/*.o
 
-gcc -m32 -std=c99 -Wall -O3 src/main/c/*.c build/dist/x86/libmpc.a build/dist/x86/libhashmap.a build/dist/x86/liblinenoise.a -lreadline -lm -o build/dist/x86/Lydrige-linux_x86_bin-v060a
+gcc -m32 -std=c99 -Wall -O3 src/main/c/*.c build/dist/x86/libmpc.a build/dist/x86/libhashmap.a build/dist/x86/liblinenoise.a -lm -o build/dist/x86/Lydrige-linux_x86_bin-v060a
